8cdb41d9965d9d993136604228db338e627babba
Hamza El Marjani <elmarjanicreation@users.noreply.github.com>
2019-10-27 13:31:25 +0100

    Created File

---
+++ Moroccan Food Recipe/Moroccan Food Recipe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moroccan Food Recipe
+//
+//  Created by Mac on 10/27/19.
+//  Copyright © 2019 El Marjani Creation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
